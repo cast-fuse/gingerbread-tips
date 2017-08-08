f705047a2932f3f3405f0856b68359382eab516e
Andrew MacMurray <a.macmurray@icloud.com>
2017-08-08 16:34:17 +0100

    add tachyons css and dummy tips

--- microsite/app/View.elm
+++ microsite/app/View.elm
@@ -1,10 +1,26 @@
 module View exposing (..)
 
-import Model exposing (..)
 import Html exposing (..)
+import Html.Attributes exposing (..)
+import Model exposing (..)
 
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ p [] [ text "Get building!" ] ]
+    div [ class "tc blue mt5" ]
+        [ h2 [ class "tracked-mega fw5 f2 ttu" ] [ text "Gingerbread Tips" ]
+        , div [ class "center mw8 ph3" ] <| List.map renderTip sampleTips
+        ]
+
+
+renderTip : String -> Html Msg
+renderTip tip =
+    div [ class "bg-light-blue mv4 pa4 tl br2" ] [ text tip ]
+
+
+sampleTips : List String
+sampleTips =
+    [ "Tip 1"
+    , "Tip 2"
+    , "Tip 3"
+    ]
