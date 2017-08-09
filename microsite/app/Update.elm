1245f4d57e474fb0a6554357b2937ee0aa929d9b
Andrew MacMurray <a.macmurray@icloud.com>
2017-08-09 11:22:07 +0100

    get data from the wordpress api, renders tips based on the remoteData status #13 #30 #32

--- microsite/app/Update.elm
+++ microsite/app/Update.elm
@@ -1,15 +1,23 @@
 module Update exposing (..)
 
+import Data.Tip exposing (handleTipsResponse)
 import Model exposing (..)
+import RemoteData exposing (..)
+import Request.Tip exposing (getTips)
 
 
 init : ( Model, Cmd Msg )
 init =
-    {} ! []
+    initialState ! [ getTips ]
+
+
+initialState : Model
+initialState =
+    { tips = Loading }
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        NoOp ->
-            model ! []
+        TipsResponse tipsResponse ->
+            (model |> handleTipsResponse tipsResponse) ! []
