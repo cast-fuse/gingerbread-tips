092182c004a4bd0991cd4edb707da676985105b6
Ivan Gonzalez <ivangonzalez@rocketmail.com>
2017-08-14 18:50:03 +0100

    Modify file paths for images and stylesheets to work with github pages

--- microsite/app/elm/Views/Navbar.elm
+++ microsite/app/elm/Views/Navbar.elm
@@ -9,6 +9,6 @@ import Model exposing (..)
 navbar : Html Msg
 navbar =
     div [ class "flex justify-between items-center mw7-ns mw8 center pa3" ]
-        [ div [ class "w-100 mw6 pointer ma3 tl", onClick Home ] [ img [ src "/img/gingerbread-logo.png", class "w-100 w-70-ns" ] [] ]
+        [ div [ class "w-100 mw6 pointer ma3 tl", onClick Home ] [ img [ src "/gingerbread-tips/img/gingerbread-logo.png", class "w-100 w-70-ns" ] [] ]
         , div [ class "bg-orange pv2 ph4 white tracked fw1 br5" ] [ h3 [ class "ma0 f6" ] [ text "Main site" ] ]
         ]
